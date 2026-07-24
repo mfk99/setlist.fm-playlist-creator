bac5cd78fd0f90c874890aaaf348dd37b897d70e
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-07-24 11:47:07 +0300

    add cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,25 @@
 import express from "express";
+import cors from "cors";
 import { scrapePage } from "./data-retriever.js";
 
 const app = express();
 const port = "3000";
+
+const allowedOrigins = ["http://localhost:5173"];
+app.use(
+  cors({
+    origin: function (origin: any, callback: any) {
+      if (!origin) return callback(null, true);
+
+      if (allowedOrigins.indexOf(origin) === -1) {
+        return callback(new Error("Not allowed by CORS"));
+      }
+
+      return callback(null, true);
+    },
+    credentials: true,
+  }),
+);
 
 app.get("/", (req, res) => {});
 
