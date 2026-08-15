5d2b50d8fff0d45794b1d02af0a218a4f01a35c8
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-08-15 18:08:15 +0300

    add debug log

--- server/src/config/env.ts
+++ server/src/config/env.ts
@@ -4,6 +4,7 @@ export const REDIRECT_URI = `${BASE_URL}/spotify/callback`;
 
 function determineBaseUrl() {
   const local = process.env.LOCAL;
+  console.log("local:", local);
   if (local) return process.env.DEV_BASE_URL;
   return process.env.PROD_BASE_URL;
 }
