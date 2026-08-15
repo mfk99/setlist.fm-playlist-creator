6d608acc7fb00d1a85814eff29a2b65963c4616a
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-08-15 16:55:37 +0300

    add auth flow button

--- client/src/components/login/SpotifyLogin.tsx
+++ client/src/components/login/SpotifyLogin.tsx
@@ -1,31 +1,11 @@
-import { useState } from "react";
-
 export function SpotifyLogin() {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  async function handleSpotifyLogin() {
-    console.log("token received");
-  }
-
+  const link = "/api/spotify/login";
   return (
     <>
-      <div>To download the list, log in to Spotify below.</div>
-      <input
-        type="text"
-        placeholder="Enter Username"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-      />
-      <input
-        type="password"
-        placeholder="Enter Password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-      />
-      <button type="submit" onClick={handleSpotifyLogin}>
-        Login
-      </button>
+      <div>To download the list, log in to Spotify.</div>
+      <a href={link}>
+        <button>Log into Spotify</button>
+      </a>
     </>
   );
 }
