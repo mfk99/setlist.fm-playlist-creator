eae0b4e232b95b7bf8ce6ddb6e84fec3bddf1b7c
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-08-15 21:05:22 +0300

    fix

--- client/src/components/services/login-service.ts
+++ client/src/components/services/login-service.ts
@@ -1,12 +1,10 @@
 import axios from "axios";
+import { BASE_URL } from "../utils/env";
 
 export async function getCredentialStatus() {
-  const response = await axios.get(
-    "https://setlist-fm-playlist-creator.vercel.app/auth/me",
-    {
-      withCredentials: true,
-    },
-  );
+  const response = await axios.get(`${BASE_URL}/auth/me`, {
+    withCredentials: true,
+  });
 
   console.log(response);
   console.log(response.data);
