fba911642f26f547f24ed8746a7cd22ae09ac2a5
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-08-15 20:24:48 +0300

    more debug logs

--- server/src/controllers/authController.ts
+++ server/src/controllers/authController.ts
@@ -3,6 +3,8 @@ import type { Request, Response } from "express";
 export default async function authentication(req: Request, res: Response) {
   const session = req.cookies.session;
 
+  console.log("cookies:", req.cookies);
+
   if (!session) {
     return res.json({
       authenticated: false,
