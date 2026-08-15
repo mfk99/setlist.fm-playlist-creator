63147d3804263f1ccb933835ea2e495a7e0c92ab
Matti Kähkönen <matti.kahkonen99@gmail.com>
2026-08-15 23:31:59 +0300

    fix routing

--- server/src/routes/spotify.ts
+++ server/src/routes/spotify.ts
@@ -5,6 +5,7 @@ import {
   callback,
   artist,
   songs,
+  playlist,
 } from "../controllers/spotifyController.js";
 
 const spotifyRouter = Router();
@@ -12,6 +13,7 @@ const spotifyRouter = Router();
 spotifyRouter.get("/login", login);
 spotifyRouter.get("/token", token);
 spotifyRouter.get("/callback", callback);
+spotifyRouter.get("/playlist", playlist);
 spotifyRouter.get("/artist/:artistId/token/:accessToken", artist);
 spotifyRouter.get("/songs/artist/:artist/token/:accessToken", songs);
 
